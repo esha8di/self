edd25bf14e66ad94f43bb37b0ccf793dc9ff148a
Esha <shuminnahar8di@gmail.com>
2026-09-07 22:46:54 +0600

    first problem

--- app/src/main/java/com/example/myapplication/Main.kt
+++ app/src/main/java/com/example/myapplication/Main.kt
@@ -1,4 +1,16 @@
-fun main(){
-    println("Hello World")
+
+fun formatDuration(minutes: Int): String {
+    val hours = minutes/60
+    val mins= minutes%60
+    return "${hours}h ${mins}m"
+
 }
 
+
+fun main(){
+    println(formatDuration(0))
+    println(formatDuration(60))
+    println(formatDuration(120))
+    println(formatDuration(135))
+    println(formatDuration(180))
+}
